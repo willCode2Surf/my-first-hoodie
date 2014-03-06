e7c6d937e1d90f7c6c4e1b98c97429fb5aa88722
Dustin W Brown <dustin.w.brown@gmail.com>
2014-03-06 05:32:15 -0600

    Update main.js

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,20 +1,20 @@
 // initialize Hoodie
-var hoodie  = new Hoodie()
+var armt  = new ARMT()
 
 // initial load of all todo items from the store
-hoodie.store.findAll('todo').then( function(todos) {
+armt.store.findAll('todo').then( function(todos) {
   todos.sort( sortByCreatedAt ).forEach( addTodo )
 })
 
 // when a new todo gets stored, add it to the UI
-hoodie.store.on('add:todo', addTodo)
+armt.store.on('add:todo', addTodo)
 // clear todo list when the get wiped from store
-hoodie.account.on('signout', clearTodos)
+armt.account.on('signout', clearTodos)
 
 // handle creating a new task
 $('#todoinput').on('keypress', function(event) {
   if (event.keyCode == 13) { // ENTER
-    hoodie.store.add('todo', {title: event.target.value});
+    armt.store.add('todo', {title: event.target.value});
     event.target.value = '';
   }
 })
